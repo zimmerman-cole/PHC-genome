fb3fea93f85e3a02777cca4c295efe042dc7a3cd
Cole Zimmerman <czimmer112@gmail.com>
2019-02-04 16:00:12 -0800

    Add one_hot_encode function to utils.py.

--- src/utils.py
+++ src/utils.py
@@ -2,10 +2,23 @@
 Utility functions.
 """
 
+import numpy as np
+
 def is_iterable(obj):
     try:
         list(obj)
         return True
     except TypeError:
         return False
+    
+def one_hot_encode(y):
+    us = np.unique(y)
+    us = sorted(us)
+    dc = dict(zip(us, range(len(us))))
+    
+    out = np.zeros((y.shape[0], len(us)))
+    for i, label in enumerate(y):
+        out[i, dc[label]] = 1
+        
+    return out, dc
    
